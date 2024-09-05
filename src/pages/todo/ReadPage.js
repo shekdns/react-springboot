5990c7224450a7326b836605574858248cafefbd
shekdns <aah03@naver.com>
2024-09-05 23:41:41 +0900

    2024/09/05 useParam study

---
+++ src/pages/todo/ReadPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const ReadPage = () => {
+  const {tno} = useParams();
+
+  return (
+    <div className='text-3xl font-extrabold'>
+      Todo Read Page Component {tno}
+    </div>
+  );
+};
+
+
+export default ReadPage;
--- src/router/root.js
+++ src/router/root.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Suspense, lazy } from "react"; //필요한 순간까지 컴포넌트를 메모리상으로 올리지 않도록 지연로딩을 위해서 사용
+import todoRouter from "./todoRouter";
 
 const Loading = <div>Loading...</div>
 const Main    = lazy( () => import( "../pages/MainPage" ) )
@@ -8,6 +9,8 @@ const About = lazy( () => import( "../pages/AboutPage" ) )
 
 //const { createBrowserRouter } = require( "react-router-dom" );
 const TodoIndex = lazy( () => import( "../pages/todo/IndexPage" ) )
+
+//const TodoList = lazy( () => import( "../pages/todo/ListPage" ) )
 
 const root = createBrowserRouter([
   {
@@ -20,7 +23,8 @@ const root = createBrowserRouter([
   },
   {
     path: "todo",
-    element : <Suspense fallback={Loading}><TodoIndex/></Suspense>
+    element : <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+    children: todoRouter()
   },
 ])
 
