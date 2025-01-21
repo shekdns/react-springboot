b5084debf1e6235656635f380a0239f6cd6250cc
shekdns <aah03@naver.com>
2025-01-21 22:00:36 +0900

    기본적인 게시판 까지

--- src/components/todo/AddComponent.jsx
+++ src/components/todo/AddComponent.jsx
@@ -57,7 +57,7 @@ const AddComponent = () => {
       <div className="flex justify-center">
         <div className="relative mb-4 flex w-full flex-wrap items-stretch">
           <div className="w-1/5 p-6 text-right font-bold">DUEDATE</div>
-          <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md" name="due_date" type={'date'} value={todo.dueDate} onChange={handleChangeTodo}>
+          <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md" name="due_date" type={'date'} value={todo.due_date} onChange={handleChangeTodo}>
           </input>
         </div>
       </div>
--- src/pages/todo/ModifyPage.jsx
+++ src/pages/todo/ModifyPage.jsx
@@ -1,23 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import ModifyComponent from "../../components/todo/ModifyComponents";
+import { useParams } from "react-router-dom";
 
-const ModifyPage = ( {tno} ) => {
-  const navigate = useNavigate()
+const ModifyPage = () => {
 
-  const moveToRead = () => {
-    navigate( { pathname:`/todo/read/${tno}` } )
-  }
-
-  const moveToList = () => {
-    navigate( { pathname:`/todo/list` } )
-  }
+  const {tno} = useParams()
 
   return (
-    <div className="text-3xl font-extrabold">
-      Todo Modify Page
-      {/* <div>
-        <button onClick={ () => moveToRead(tno) }>Test Read</button>
-        <button onClick={ () => moveToList() }>Test List</button>
-      </div> */}
+    <div className="p-4 w-full bg-white">
+      <div className="text-3xl font-extrabold">
+        Todo Modify Page
+      </div>
+      <ModifyComponent tno={tno}/>
     </div>
   );
 }
